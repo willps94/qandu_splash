0651c46b7219f5d3adfd51a87aefa7b42b015562
Will <wtprattstephen@quinnipiac.edu>
2015-09-17 16:14:37 +0000

    Setup Heroku

--- qandu_splash/wsgi.py
+++ qandu_splash/wsgi.py
@@ -14,5 +14,5 @@ from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qandu_splash.settings")
 
-application = Cling(get_wsgi_application()
+application = Cling(get_wsgi_application())
 
